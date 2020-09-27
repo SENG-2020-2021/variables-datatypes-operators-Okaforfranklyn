15c5020238d0f031102da9e6efcde6f1d27795ed
Okaforfranklyn <okafor0948@student.babcock.edu.ng>
2020-09-27 16:34:48 +0000

    the assignment was on a microfinance bank that, gave out loan. this code is about a debtor and how he is paying, what he is owes and what is left for him to balance the bank.

--- Main.java
+++ Main.java
@@ -1,21 +1,27 @@
 class Main {
   public static void main(String[] args) {
-   // kindly use appropriate data types for your declaration
-   // declare variables days the debtor defaulted, amount the debtor is to pay per day, amount the debtor is to pay and assign values to them
    
-   //Declare the variable of the total amount the debtor is to pay, the days the debtor paid for, amount the debtor is left to pay, days the debtor has not payed for
+   int daysdebtorfailed = 4;
+   int amountdebtorpaysperday = 500;
+   int amountpaid = 1000;
 
-   // calculate and print total amount the debtor is to pay
+   int totalamounttopay;
+   int daysthedebtorpaid;
+   int amountremaining;
+   int daysdebtordidntpay;
 
-   // calculate and print the days the debtor paid for
-  
-  // calculate and print the amount whose day was not captured because the amount was incomplete
+   totalamounttopay = daysdebtorfailed * amountdebtorpaysperday;
+   System.out.println("the total amount the debtor is to pay" + totalamounttopay);
 
-   // calculate and print amount the debtor is left to pay
+   daysthedebtorpaid = amountpaid/amountdebtorpaysperday;
+   System.out.println("days debtor paid for: " + daysthedebtorpaid );
 
-   // calculate and print days the debtor has not payed for
+   amountremaining = amountpaid % amountdebtorpaysperday;
+   System.out.println("remaining debt the debtor to pay " + amountremaining);
 
-   //kindly remove the statement below when you are done with the assignment
-    System.out.println("Hello world!");
+   daysdebtordidntpay = daysdebtorfailed - daysthedebtorpaid;
+   System.out.println ("days debtor havent paid "+ daysdebtordidntpay);
+
+
   }
 }
